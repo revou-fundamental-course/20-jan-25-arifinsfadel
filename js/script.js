a187a6f3815c2143a5970ca9acc5bff2a5257364
arifinsfadel <33124706+arifinsfadel@users.noreply.github.com>
2025-01-30 23:56:09 +0700

    Last Update Version 1.0

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 // Ini File JavaScript
+
+
+
+// Fungsi untuk memvalidasi input berat badan, tinggi badan, usia, dan gender
+
+  function calculateBmi() {
+    let resultBmi = document.getElementById('result-bmi');
+    let weight = document.getElementById('input-berat-badan').value;
+    let height = document.getElementById('input-tinggi-badan').value;
+    let age = document.getElementById('input-usia').value;
+
+    // Fungsi untuk menghitung BMI berdasarkan berat badan dan tinggi badan
+
+    if (weight != '' && height != '' && age != '') {
+        let bmi = (weight / ((height / 100) ** 2)).toFixed(2);
+        resultBmi.textContent = bmi;
+    } else {
+        alert('Tolong isi all fields!');
+    }
+}
+
+
